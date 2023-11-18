567012eea4cf869ada56e87c51955768d6c7e574
Erwin Gunawan <erwin.gunawan@binus.ac.id>
2023-11-18 22:25:07 +0700

    Update RPS.py

--- RockPaperScissor/RPS.py
+++ RockPaperScissor/RPS.py
@@ -1,11 +1,43 @@
 # The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge .
+#Single Method Example
+#reference: https://medium.com/@sri.hartini/rock-paper-scissors-in-python-5173ab69ca7a
+def player(prev_play, opponent_history=[], play_order={}):
+    if not prev_play:
+        prev_play = 'R'
+
+    opponent_history.append(prev_play)
+    #init prediciton to opponent moves
+    prediction = 'P'
+    
+    if len(opponent_history) > 4:
+        last_five = "".join(opponent_history[-5:])
+        play_order[last_five] = play_order.get(last_five, 0) + 1
+        
+        potential_plays = [
+            "".join([*opponent_history[-4:], v]) 
+            for v in ['R', 'P', 'S']
+        ]
+
+        sub_order = {
+            k: play_order[k]
+            for k in potential_plays if k in play_order
+        }
+
+        if sub_order:
+            prediction = max(sub_order, key=sub_order.get)[-1:]
+        else:
+            prediction = max(play_order, key=play_order.get)[-1:]
+
+    ideal_response = {'P': 'S', 'R': 'P', 'S': 'R'}
+
+    return ideal_response[prediction]
 
 # Proposed Method:
   # since we know that the test_module is testing in this order : abbey -> kris -> mrugesh -> quincy, so we will use counter_global as the check point to switch logic
 
 counter_global=[0]
 
-def player(prev_play, opponent_history=[], counter=[0]):
+def player1(prev_play, opponent_history=[], counter=[0]):
   global counter_global
   
   if counter_global[0] % 1000 == 0:
